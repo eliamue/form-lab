8d3c0f9dd8d1fbba86b0ee140499259cc345dd4d
Elise Muellerleile <eliamue@live.com>
2021-09-09 22:58:38 -0500

    newsApi added

---
+++ src/services/newsApi.js
@@ -0,0 +1,26 @@
+export const formatArticles = (json) =>
+  json.map(article => ({
+    title: article.title,
+    article: article.article,
+    image: article.image
+  }));
+
+export const fetchArticles = async () => {
+  const res = await fetch(
+    // eslint-disable-next-line max-len
+    'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=dbd0e127d865484e89ba1f1171627bfb'
+  );
+  const json = await res.json();
+
+  return formatArticles(json);
+};
+
+export const fetchArticlesByTitle = async (articleTitle) => {
+  const res = await fetch(
+    // eslint-disable-next-line max-len
+    `https://newsapi.org/v2/everything?qInTitle=${articleTitle}&apiKey=dbd0e127d865484e89ba1f1171627bfb`
+  );
+  const json = await res.json();
+
+  return formatArticles(json);
+};
